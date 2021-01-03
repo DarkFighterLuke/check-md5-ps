b522dede5b52c00385feefa8e4b78f015545f3f8
DarkFighterLuke <luca-consoli@live.it>
2021-01-03 23:07:32 +0100

    Switched to MergeSort+BinarySearch

--- check-md5.ps1
+++ check-md5.ps1
@@ -31,6 +31,113 @@ function generateResumeData{
     return
 }
 
+$global:counter = 0 
+ 
+# Merges two sorted halves of a subarray 
+# $theArray is an array of comparable objects 
+# $tempArray is an array to place the merged result 
+# $leftPos is the left-most index of the subarray 
+# $rightPos is the index of the start of the second half 
+# $rightEnd is the right-most index of the subarray 
+function merge($theArray, $tempArray, [int] $leftPos, [int] $rightPos, [int] $rightEnd) 
+{ 
+    $leftEnd = $rightPos - 1 
+    $tmpPos = $leftPos 
+    $numElements = $rightEnd - $leftPos + 1 
+     
+    # Main loop 
+    while (($leftPos -le $leftEnd) -and ($rightPos -le $rightEnd)) 
+    { 
+        $global:counter++ 
+        if ($theArray[$leftPos].CompareTo($theArray[$rightPos]) -le 0) 
+        { 
+            $tempArray[$tmpPos++] = $theArray[$leftPos++] 
+        } 
+        else 
+        { 
+            $tempArray[$tmpPos++] = $theArray[$rightPos++] 
+        } 
+    } 
+     
+    while ($leftPos -le $leftEnd) 
+    { 
+        $tempArray[$tmpPos++] = $theArray[$leftPos++] 
+    } 
+     
+    while ($rightPos -le $rightEnd) 
+    { 
+        $tempArray[$tmpPos++] = $theArray[$rightPos++] 
+    } 
+     
+    # Copy $tempArray back 
+    for ($i = 0; $i -lt $numElements; $i++, $rightEnd--) 
+    { 
+        $theArray[$rightEnd] = $tempArray[$rightEnd] 
+    } 
+} 
+ 
+# Makes recursive calls 
+# $theArray is an array of comparable objects 
+# $tempArray is an array to place the merged result 
+# $left is the left-most index of the subarray 
+# $right is the right-most index of the subarray 
+function mergesorter( $theArray, $tempArray, [int] $left, [int] $right ) 
+{ 
+    if ($left -lt $right) 
+    { 
+        [int] $center = [Math]::Floor(($left + $right) / 2) 
+        mergesorter $theArray $tempArray $left $center 
+        mergesorter $theArray $tempArray ($center + 1) $right 
+        merge $theArray $tempArray $left ($center + 1) $right 
+    } 
+}
+
+function Binary-Search {
+    Param (
+        [Parameter(Mandatory=$True)
+        ]
+
+        $InputArray,
+        $SearchVal,
+        $Attribute)
+
+    $LowIndex = 0                              #Low side of array segment
+    $Counter = 0
+    $TempVal = ""                              #Used to determine end of search where $Found = $False
+    $HighIndex = $InputArray.count             #High Side of array segment
+    [int]$MidPoint = ($HighIndex-$LowIndex)/2  #Mid point of array segment
+    $found = $False
+
+
+    While($LowIndex -le $HighIndex){
+        $MidVal = $InputArray[$MidPoint]
+                                                    
+        If($TempVal -eq $MidVal){              #If identical, the search has completed and $Found = $False
+            $found = $False
+            Return
+        }
+        else{
+            $TempVal = $MidVal                 #Update the TempVal. Search continues.
+        }
+        
+        If($SearchVal -lt $MidVal) {
+            $Counter++
+            $HighIndex = $MidPoint 
+            [int]$MidPoint = (($HighIndex-$LowIndex)/ 2 +$LowIndex)
+        }
+        If($SearchVal -gt $MidVal) {
+            $Counter++
+            $LowIndex = $MidPoint 
+            [int]$MidPoint = ($MidPoint+(($HighIndex - $MidPoint) / 2))         
+        }
+        If($SearchVal -eq $MidVal) {
+            $found = $True 
+            break
+        }
+    }
+    return $found
+}
+
 if(Test-Path $scriptDataPath -PathType Leaf){
     Write-Output "Parsing resume data..."
     $resumeData=Get-Content $scriptDataPath
@@ -60,31 +167,23 @@ else{
     generateResumeData
 }
 
-#TODO: Switch to mergesort + binary research
-#TODO: Make the comparison multithread dividing file1 records in n_cpu_cores parts
+Write-Host "Sorting second file with MergeSort..."
+$tempArray = New-Object Object[] $file2.Count 
+mergesorter $file2 $tempArray 0 ($file2.Count - 1) 
+Write-Host "MergeSort finished."
+
 Write-Output "`t--- Start of comparison ---`n"
 for($i=$startIndex; $i -lt $file1.length; $i++){
-    $line1=$file1[$i]
-    $filename1=$line1.Substring(34)
-    foreach($line2 in $file2){
-        $filename2=$line2.Substring(34)
-        if($filename1 -eq $filename2){
-            $md51=$line1.Substring(0, 32)
-            $md52=$line2.Substring(0, 32)
-            if($md51 -ne $md52){
-                Write-Output $($md51+"  "+$md52+"`t"+$filename1+"  NO!")| Out-File -Path $results -Append
-            }
-            else{
-                Write-Output $($md51+"`t"+$filename1+"  OK"+"`t`t["+$($i+1)+"/"+$totalLines+"]")
-            }
-            $matchFound=$True;
-        }
-        elseif(-not $matchFound){
-            Write-Host $($filename1+":`tRow not found!") | Out-File -Path $results -Append
-        }
+    $found=Binary-Search $file2 $file1[$i]
+    if($found){
+        Write-Output $($filename1+"  OK"+"`t`t["+$($i+1)+"/"+$totalLines+"]")
+    }
+    else{
+        Write-Output $($file1[$i]+"  NO!"+"`t`t["+$($i+1)+"/"+$totalLines+"]")| Out-File -Path $results -Append
     }
     $resumeData[2]=$i
     $resumeData | Out-File -Path $scriptDataPath
 }
+
 Write-Output "`n`t--- End of comparison ---"
 Remove-Item $scriptDataPath -Force
